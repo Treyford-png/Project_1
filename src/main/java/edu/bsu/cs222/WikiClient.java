d0d9bd0877482f96238d4c4458661f191e27f20b
holdenghankins <hghankins@gmail.com>
2026-02-11 16:27:29 -0500

    Comments added to WikiClient.java

--- src/main/java/edu/bsu/cs222/WikiClient.java
+++ src/main/java/edu/bsu/cs222/WikiClient.java
@@ -14,17 +14,19 @@ public class WikiClient {
             "RevisionReporters/0.1 (Holden, Treyford)";
 
     public String fetchArticleJson(String urlString) throws IOException {
+        // Connects to Wikipedia article's API using URL obtained in WikiUrlBuilder
         URL url = new URL(urlString);
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent", USER_AGENT);
 
+        // Reads stream directly from Wikipedia API
         try (InputStream inputStream = connection.getInputStream();
              BufferedReader reader = new BufferedReader(
                      new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
 
+            // Creates JSON from stream
             StringBuilder json = new StringBuilder();
             String line;
-
             while ((line = reader.readLine()) != null) {
                 json.append(line);
             }
