bb763ff126b997a87de28f271b1c64a076a2d108
Treyford Mercer <thwmercer@gmail.com>
2026-02-17 19:30:41 -0500

    Created GUI input with GUI output, displays basic string response in controller as output for now.

--- src/main/java/edu/bsu/cs222/Controller.java
+++ src/main/java/edu/bsu/cs222/Controller.java
@@ -1,4 +1,28 @@
 package edu.bsu.cs222;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    public void Method(ActionEvent actionEvent) {
+        String output = getOutput();
+        Label label = new Label(output);
+        StackPane stack = new StackPane(label);
+        Scene scene = new Scene(stack, 300, 300);
+        Stage popup = new Stage();
+        popup.setTitle("Placeholder");
+        popup.setScene(scene);
+        popup.show();
+    }
+
+    public String getOutput(){
+        return "this is an output";
+    }
 }
